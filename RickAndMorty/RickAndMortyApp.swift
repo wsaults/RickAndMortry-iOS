e8cb872172ed0c02c1d8808bf58e55b61a04ba43
Will Saults <will@saults.io>
2025-09-29 18:36:25 -0500

    Sets up initial project structure
    Initializes the project with basic files and configurations
    for the Rick and Morty app.
    
    Adds project files, default assets, a basic content view, and
    app delegate. Also includes initial testing and UI testing
    configurations.
    
    Adds .DS_Store to .gitignore.

---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Will Saults on 9/29/25.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RickAndMortyTests/RickAndMortyTests.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTests.swift
+//  RickAndMortyTests
+//
+//  Created by Will Saults on 9/29/25.
+//
+
+import Testing
+@testable import RickAndMorty
+
+struct RickAndMortyTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
